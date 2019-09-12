a5b4cdaa928afe1ff5ae58ff1751d22067ac4be8
leinwanderIstvan <coolcodebasic@gmail.com>
2019-09-12 14:05:55 +0200

    New supplier, product category changed

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -33,34 +33,44 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(monyo);
         Supplier szentAndrasSerfozde = new Supplier("Szent Andras Brewing","Beer");
         supplierDataStore.add(szentAndrasSerfozde);
-        Supplier craftwater = new Supplier("Simple Water","Wter");
+        Supplier craftwater = new Supplier("Simple Water","Water");
         supplierDataStore.add(craftwater);
+        Supplier codecool = new Supplier("CodeCool", "Beer");
+        supplierDataStore.add(codecool);
 
 
         //setting up a new product category
-        ProductCategory beer = new ProductCategory("Craft Beer", "Beer", "Beer is a teast flued witch make you smile");
-        productCategoryDataStore.add(beer);
-        ProductCategory other = new ProductCategory("Craft Water", "Water", "Water is good for you");
-        productCategoryDataStore.add(other);
+        ProductCategory ipa = new ProductCategory("IPA", "Beer", "Beer is a teast flued witch make you smile");
+        productCategoryDataStore.add(ipa);
+        ProductCategory apa = new ProductCategory("APA", "Beer", "Beer is a teast flued witch make you smile");
+        productCategoryDataStore.add(apa);
+        ProductCategory stout = new ProductCategory("Stout","Beer","Beer is a teast flued witch make you smile");
+        productCategoryDataStore.add(stout);
+        ProductCategory lager = new ProductCategory("Lager", "Beer", "Beer is a teast flued witch make you smile");
+        productCategoryDataStore.add(lager);
+        ProductCategory water =  new ProductCategory("Water", "Water","Water is health!");
+        productCategoryDataStore.add(water);
 
 
         //setting up products and printing it
-        productDataStore.add(new Product("Fake Coffee", 650, "HUF", "Stout style black craft beer with good old coffee taste", beer, firstCraftBeer));
-        productDataStore.add(new Product("Tropical Ipa", 750, "HUF", "A FIRST Tropical IPA  with awsome tropical taste", beer, firstCraftBeer));
-        productDataStore.add(new Product("Twisted Pils", 550, "HUF", "Traditional czech lager craft beer.", beer, firstCraftBeer));
-        productDataStore.add(new Product("Kohatu Kohatu",1200,"HUF","Single hop NEw england IPA",beer,firstCraftBeer));
-        productDataStore.add(new Product("Milk Stout",900,"HUF","Black stout beer",beer,legenda));
-        productDataStore.add(new Product("Black Jack",500,"HUF","Good old hungarian IPA",beer,legenda));
-        productDataStore.add(new Product("Buldozer",700,"HUF","Simple IPA.",beer,legenda));
-        productDataStore.add(new Product("Six Fingers",800,"HUF","Harmonic taste in weeise beer",beer,legenda));
-        productDataStore.add(new Product("American Beauty",750,"HUF","American Pale Ale",beer,monyo));
-        productDataStore.add(new Product("Black Mamba",950,"HUF","Black milk stout!",beer,monyo));
-        productDataStore.add(new Product("Flying Rabbit",650,"HUF","American IPA",beer,monyo));
-        productDataStore.add(new Product("Schatzi",800,"HUF","Simple Lager",beer,monyo));
-        productDataStore.add(new Product("Bandiba",650,"HUF","Tasty American IPA",beer,szentAndrasSerfozde));
-        productDataStore.add(new Product("Esthajnal'18",1000,"HUF","Strong stout with plum flavoring",beer,szentAndrasSerfozde));
-        productDataStore.add(new Product("Ogre",540,"HUF","Strong tasty lager",beer,szentAndrasSerfozde));
-        productDataStore.add(new Product("Water",0,"HUF","This is a beer shop!",other,craftwater));
+        productDataStore.add(new Product("Fake Coffee", 650, "HUF", "Stout style black craft beer with good old coffee taste", stout, firstCraftBeer));
+        productDataStore.add(new Product("Tropical Ipa", 750, "HUF", "A FIRST Tropical IPA  with awsome tropical taste", ipa, firstCraftBeer));
+        productDataStore.add(new Product("Twisted Pils", 550, "HUF", "Traditional czech lager craft beer.", lager, firstCraftBeer));
+        productDataStore.add(new Product("Kohatu Kohatu",1200,"HUF","Single hop New england IPA",ipa,firstCraftBeer));
+        productDataStore.add(new Product("Milk Stout",900,"HUF","Black stout beer",stout,legenda));
+        productDataStore.add(new Product("Black Jack",500,"HUF","Good old hungarian IPA",ipa,legenda));
+        productDataStore.add(new Product("Buldozer",700,"HUF","Simple IPA.",ipa,legenda));
+        productDataStore.add(new Product("Six Fingers",800,"HUF","Harmonic taste in weeise beer",lager,legenda));
+        productDataStore.add(new Product("American Beauty",750,"HUF","American Pale Ale",apa,monyo));
+        productDataStore.add(new Product("Black Mamba",950,"HUF","Black milk stout!",stout,monyo));
+        productDataStore.add(new Product("Flying Rabbit",650,"HUF","American IPA",ipa,monyo));
+        productDataStore.add(new Product("Schatzi",800,"HUF","Simple Lager",lager,monyo));
+        productDataStore.add(new Product("Bandiba",650,"HUF","Tasty American IPA",ipa,szentAndrasSerfozde));
+        productDataStore.add(new Product("Esthajnal'18",1000,"HUF","Strong stout with plum flavoring",stout,szentAndrasSerfozde));
+        productDataStore.add(new Product("Ogre",540,"HUF","Strong tasty lager",lager,szentAndrasSerfozde));
+        productDataStore.add(new Product("CodeCool Beer",900,"HUF","Tasty IPA from the best!",ipa,codecool));
+        productDataStore.add(new Product("Tap water",15000,"HUF","Quality from wall!",water,craftwater));
+
 
     }
 }
