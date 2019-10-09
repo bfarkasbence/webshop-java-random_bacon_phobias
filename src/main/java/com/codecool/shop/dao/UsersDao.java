1fefcc781aac2a675399e7b735803df1da5099a4
Benyo <b.farkasbence@gmail.com>
2019-10-09 10:25:13 +0200

    created User model, UseresDaoJdbc and UsersDao interface

---
+++ src/main/java/com/codecool/shop/dao/UsersDao.java
@@ -0,0 +1,7 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.dao.UsersDao;
+
+
+public interface UsersDao {
+}
---
+++ src/main/java/com/codecool/shop/dao/implementation/UsersDaoJdbc.java
@@ -0,0 +1,5 @@
+package com.codecool.shop.dao.implementation;
+
+
+public class UsersDaoJdbc {
+}
